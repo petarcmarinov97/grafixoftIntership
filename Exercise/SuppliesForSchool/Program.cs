66b62394225774eed8fe8e074cdf2b1ee8f21bd5
Petar Marinov <petar.marinov@grafixoft.com>
2021-11-10 17:51:24 +0200

    First Step Exercise and Lab are done

---
+++ Exercise/SuppliesForSchool/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SuppliesForSchool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pensCount = int.Parse(Console.ReadLine());
+            int markersCount = int.Parse(Console.ReadLine());
+            int litresForDesk = int.Parse(Console.ReadLine());
+            int percentDiscount = int.Parse(Console.ReadLine());
+
+            double pensPackage = pensCount * 5.80;
+            double markersPackage = markersCount * 7.20;
+            double deskCleaningPackage = litresForDesk * 1.20;
+            double price = pensPackage + markersPackage + deskCleaningPackage;
+
+            double finalPrice = price - (price * percentDiscount / 100);
+
+            Console.WriteLine(finalPrice);
+        }
+    }
+}
