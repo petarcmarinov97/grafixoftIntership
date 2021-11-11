1caddaaace0d238ed2042e667a6e658e20daeb41
Petar Marinov <petar.marinov@grafixoft.com>
2021-11-11 12:25:22 +0200

    First Steps Exercise and Lab

---
+++ FirstSteps/Exercise/FoodDelivery/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FoodDelivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceDelivery = 2.50;
+            int chickenMenus = int.Parse(Console.ReadLine());
+            int fishMenus = int.Parse(Console.ReadLine());
+            int vegeterianMenus = int.Parse(Console.ReadLine());
+
+            double chickenMenusPrice = chickenMenus * 10.35;
+            double fishMenusPrice = fishMenus * 12.40;
+            double vegeterianMenusPrice = vegeterianMenus * 8.15;
+
+            double fullSumWithoutDelivery = chickenMenusPrice + fishMenusPrice + vegeterianMenusPrice;
+            double dessert = fullSumWithoutDelivery * 0.2;
+
+            double finalPrice = fullSumWithoutDelivery + dessert + priceDelivery;
+
+            Console.WriteLine(finalPrice);
+
+        }
+    }
+}
