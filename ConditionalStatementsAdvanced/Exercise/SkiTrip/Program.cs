d4525dc112ad2c7ec1775b6c80fac573c0f9431b
Petar Marinov <petar.marinov@grafixoft.com>
2021-11-12 15:50:11 +0200

    Updated ConditionalStatementsAdvanced

--- ConditionalStatementsAdvanced/Exercise/SkiTrip/Program.cs
+++ ConditionalStatementsAdvanced/Exercise/SkiTrip/Program.cs
@@ -10,28 +10,24 @@ namespace ConsoleApp6
             double nights = days - 1;
             string roomType = Console.ReadLine();
             string rating = Console.ReadLine();
-            double price = 0.0;
-            double totalPrice = 0.0;
-            double totalPriceSecond = 0.0;
-            double finalPrice = 0.0;
-            double percent = 0.0;
+            double price;
+            double priceAfterDiscount;
+            double currentPrice = 0.0;
+            double finalPrice;
+            double percent = 0;
             double possitive = 0.25;
             double negative = 0.1;
 
             if (roomType == "room for one person")
             {
                 price = 18.00;
-                totalPriceSecond = nights * price;
-
-                totalPrice = rating == "positive" ? totalPriceSecond + totalPriceSecond * possitive : totalPrice = totalPriceSecond - totalPriceSecond * negative;
-                Console.WriteLine("{0:F2}", totalPrice);
-
+                currentPrice = nights * price;
             }
             else if (roomType == "apartment")
             {
                 price = 25.00;
                 percent = 0.3;
-                totalPriceSecond = nights * price;
+                currentPrice = nights * price;
 
                 if (days >= 10 && days <= 15)
                 {
@@ -41,17 +37,12 @@ namespace ConsoleApp6
                 {
                     percent = 0.5;
                 }
-                totalPrice = totalPriceSecond - totalPriceSecond * percent;
-
-                finalPrice = rating == "positive" ? totalPrice + totalPrice * possitive : totalPrice - totalPrice * negative;
-                Console.WriteLine("{0:F2}", finalPrice);
             }
-
             else if (roomType == "president apartment")
             {
                 price = 35.00;
                 percent = 0.1;
-                totalPriceSecond = nights * price;
+                currentPrice = nights * price;
 
                 if (days >= 10 && days <= 15)
                 {
@@ -61,11 +52,14 @@ namespace ConsoleApp6
                 {
                     percent = 0.20;
                 }
-                totalPrice = totalPriceSecond - totalPriceSecond * percent;
-
-                finalPrice = rating == "positive" ? totalPrice + totalPrice * possitive : totalPrice - totalPrice * negative;
-                Console.WriteLine("{0:F2}", finalPrice);
             }
+            priceAfterDiscount = currentPrice - currentPrice * percent;
+
+            finalPrice = rating == "positive" 
+                ? priceAfterDiscount + priceAfterDiscount * possitive 
+                : priceAfterDiscount - priceAfterDiscount * negative;
+
+            Console.WriteLine("{0:F2}", finalPrice);
         }
     }
 }
