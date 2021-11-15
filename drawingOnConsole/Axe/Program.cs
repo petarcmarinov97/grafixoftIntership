a90b988578b4da6ae8326c273e296961fbe9cedd
Petar Marinov <petar.marinov@grafixoft.com>
2021-11-15 18:55:57 +0200

    Initial Commit of Axe Exercise

---
+++ drawingOnConsole/Axe/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Axe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int rows = n % 2 == 1 ? (n * 2) - 1 : n * 2;
+            int handleRow = n + 1;
+
+            int dashes = n * 3;
+            string dash = new string('-', dashes);
+            string firstHalf = dash + "**";
+            string secondHalf = new string('-', n * 5 - dashes - 2);
+            string firstLine = firstHalf + secondHalf;
+            Console.WriteLine(firstLine);
+
+        }
+    }
+}
