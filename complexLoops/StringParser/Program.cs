32df47d47ea9a3335335711f06dd6a77eec84967
Petar Marinov <petar.marinov@grafixoft.com>
2021-11-16 17:17:45 +0200

    StringParser

---
+++ complexLoops/StringParser/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data;
+
+namespace StringParser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var result = new DataTable().Compute(input, null);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        }
+    }
+}
