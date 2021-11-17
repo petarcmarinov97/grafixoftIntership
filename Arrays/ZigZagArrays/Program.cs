62ced66be7b2ee7945701b3e3a57bfdb7201cd51
Petar Marinov <petar.marinov@grafixoft.com>
2021-11-17 17:25:48 +0200

    Edited ZigZayArray Exercise Changed the output from arrays to strings

--- Arrays/ZigZagArrays/Program.cs
+++ Arrays/ZigZagArrays/Program.cs
@@ -12,36 +12,27 @@ namespace ZigZagArrays
             int[] arr1 = new int[n];
             int[] arr2 = new int[n];
 
+            string string1 = "";
+            string string2 = "";
+
             for (int i = 0; i < n; i++)
             {
                 //Reading the line of two values and spliting them by ' '
-                int [] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-
-
+                int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
                 if (i % 2 == 1)
                 {
-                    arr2[i] = input[0];
-                    arr1[i] = input[1];
+                    string2 += input[0].ToString() + " ";
+                    string1 += input[1].ToString() + " ";
                 }
                 else
                 {
-                    arr2[i] = input[1];
-                    arr1[i] = input[0];
+                    string2 += input[1].ToString() + " ";
+                    string1 += input[0].ToString() + " ";
                 }
-               
             }
 
-            //Printing the arrays
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write(arr1[i] + " ");
-            }
-            Console.WriteLine();
-
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write(arr2[i] + " ");
-            }
+            Console.WriteLine(string1);
+            Console.WriteLine(string2);
         }
     }
 }
