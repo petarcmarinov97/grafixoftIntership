b89d5bcc73aa7e42753e5cbd06c63ad0edc4e52a
Petar Marinov <petar.marinov@grafixoft.com>
2021-11-18 10:30:18 +0200

    KaminoFactory Exercise

--- Arrays/KaminoFactoryStar/Program.cs
+++ Arrays/KaminoFactoryStar/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace KaminoFactoryStar
 {
@@ -6,7 +7,71 @@ namespace KaminoFactoryStar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Variables
+            int sequenceLength = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            int[] dna = new int[sequenceLength];
+            int length = 0;
+            int index = 0;
+            int sum = 0;
+            int row = 0;
+            int currentRow = 0;
+
+
+            while (input != "Clone them!")
+            {
+                //Spliting the string by "!" and remove the empty spaces
+                int[] currentDna = input.Split('!', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                currentRow++;
+
+                //Calculating the sum of the numbers per line
+                int currentSum = currentDna.Sum();
+
+
+                int currentLength = 0;
+                int currentIndex = 0;
+
+                for (int i = 0; i < currentDna.Length; i++)
+                {
+                    if (currentDna[i] == 1)
+                    {
+                        //Increasing the 1 subsequence length
+                        currentLength++;
+
+                        if (currentLength == 1)
+                        {
+                            currentIndex = i;
+                        }
+
+                        if (currentLength > length || currentLength == length && (currentIndex < index || currentSum > sum))
+                        {
+                            length = currentLength;
+                            index = currentIndex;
+                            row = currentRow;
+
+                            //Saving the current array into the dna array
+                            dna = currentDna;
+                            sum = currentSum;
+                        }
+                    }
+                    else
+                    {
+                        currentIndex = 0;
+                        currentLength = 0;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (row == 0)
+            {
+                row = 1;
+            }
+
+            Console.WriteLine($"Best DNA sample {row} with sum: {sum}.");
+            Console.WriteLine(string.Join(" ", dna));
         }
     }
 }
