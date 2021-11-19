e8c1e177c4434f3176c6d7ed23460c9458a88d84
Petar Marinov <petar.marinov@grafixoft.com>
2021-11-19 12:08:01 +0200

    All the Exercise Are Done for Methods

---
+++ MoreExerciseMethods/MultiplicationSign/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MultiplicationSign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double num3 = double.Parse(Console.ReadLine());
+
+            Sign(num1, num2, num3);
+        }
+
+        static void Sign(double n1, double n2, double n3)
+        {
+            if(n1 == 0 || n2 == 0 || n3 == 0)
+            {
+                Console.WriteLine("zero");
+            }
+            else if((n1> 0 && n2>0 && n3>0) || (n1 < 0 && n2 < 0 && n3 > 0) || (n1 < 0 && n2 > 0 && n3 < 0) || (n1 > 0 && n2 < 0 && n3 < 0))
+            {
+                Console.WriteLine("positive");
+            }
+            else
+            {
+                Console.WriteLine("negative");
+            }
+        }
+    }
+}
---
+++ MoreExerciseMethods/TribonacciSequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TribonacciSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            GetTribonaci(number);
+        }
+
+        static void GetTribonaci(int number)
+        {
+            int n1 = 1;
+            int n2 = 1;
+            int n3 = 2;
+            int nextTerm;
+
+            for(int i=1;i<=number; i++)
+            {
+                Console.Write(n1 + " ");
+                nextTerm = n1 + n2 + n3;
+                n1 = n2;
+                n2 = n3;
+                n3 = nextTerm;
+            }
+        }
+    }
+}
