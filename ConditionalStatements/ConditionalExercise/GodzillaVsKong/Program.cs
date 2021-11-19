e03555f19299742b3e54560b07bed69d0584a5a5
Petar Marinov <petar.marinov@grafixoft.com>
2021-11-19 12:34:35 +0200

    Revert "Updated"
    This reverts commit c620a97f0900c57433ccac7e0c9b33934ed5f025.

---
+++ ConditionalStatements/ConditionalExercise/GodzillaVsKong/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GodzillaVsKong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double movieBudget = double.Parse(Console.ReadLine());
+            int mutesCount = int.Parse(Console.ReadLine());
+            double suitPrice = double.Parse(Console.ReadLine());
+
+            double decorPrice = 0.10 * movieBudget;
+            double totalSuitPrice = suitPrice * mutesCount;
+
+            if (mutesCount > 150)
+            {
+                totalSuitPrice -= 0.10 * totalSuitPrice;
+            }
+
+            double totalMoney = totalSuitPrice + decorPrice;
+            double moneyLeft = movieBudget - totalMoney;
+
+            if (totalMoney > movieBudget)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {Math.Abs(moneyLeft):F2} leva more.");
+            }
+            else if (totalMoney <= movieBudget)
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {moneyLeft:F2} leva left.");
+            }
+        }
+    }
+}
---
+++ ConditionalStatements/ConditionalExercise/WorldSwimmingRecord/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WorldSwimmingRecord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double record = double.Parse(Console.ReadLine());
+            double rangeM = double.Parse(Console.ReadLine());
+            double timeSec = double.Parse(Console.ReadLine());
+
+            double rangeToSwim = rangeM * timeSec;
+
+            double fullSecs = Math.Floor(rangeM / 15) * 12.5;
+            double fullTime = rangeToSwim + fullSecs;
+
+            if (record > fullTime)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {fullTime:F2} seconds.");
+            }
+            else
+            {
+                double notEnough = fullTime - record;
+                Console.WriteLine($"No, he failed! He was {notEnough:F2} seconds slower.");
+            }
+            }
+    }
+}
