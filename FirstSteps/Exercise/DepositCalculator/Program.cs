5f360ae6cc7e0615e92d6c3f1b3e4a67419f5583
Petar Marinov <petar.marinov@grafixoft.com>
2021-11-19 13:16:12 +0200

    Fixed issues

---
+++ FirstSteps/Exercise/DepositCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DepositCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositValue = double.Parse(Console.ReadLine());
+            int monthsDeposit = int.Parse(Console.ReadLine());
+            double yearPercent = double.Parse(Console.ReadLine());
+
+            double accumulatedValue = depositValue * (yearPercent/100);
+            double forOneMonth = accumulatedValue / 12;
+            double finalResult = depositValue + (monthsDeposit * forOneMonth);
+
+            Console.WriteLine(finalResult);
+
+        }
+    }
+}
