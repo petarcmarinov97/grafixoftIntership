234159339fd375c7688c984c21a24dbe95cc0aa0
Petar Marinov <petar.marinov@grafixoft.com>
2021-11-19 12:40:02 +0200

    Fixing issues

---
+++ Exercise/Exercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double usd = double.Parse(Console.ReadLine());
+           Console.WriteLine(usd* 1.79549);
+        }
+    }
+}
