270e16d815e68746b27c5342e4d10d9a1e1eb44d
Petar Marinov <petar.marinov@grafixoft.com>
2021-11-22 12:34:44 +0200

    Lists Lab Exercises are Done

---
+++ Lists/ListManipulationAdvanced/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListManipulationAdvanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+
+            List<string> command = Console.ReadLine()
+                .Split(' ')
+                .ToList();
+
+            bool isChanged = false;
+
+            while (command[0] != "end")
+            {
+                if (command[0] == "Add")
+                {
+                    list.Add(int.Parse(command[1]));
+                    isChanged = true;
+                }
+                if (command[0] == "Remove")
+                {
+                    list.Remove(int.Parse(command[1]));
+                    isChanged = true;
+
+                }
+                if (command[0] == "RemoveAt")
+                {
+                    list.RemoveAt(int.Parse(command[1]));
+                    isChanged = true;
+
+                }
+                if (command[0] == "Insert")
+                {
+                    list.Insert(int.Parse(command[2]), int.Parse(command[1]));
+                    isChanged = true;
+
+                }
+                if (command[0] == "Contains")
+                {
+                    if (list.Contains(int.Parse(command[1])))
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No such number");
+                    }
+                }
+                if (command[0] == "PrintOdd")
+                {
+                    Console.WriteLine(string.Join(" ", list.Where(x => x % 2 == 1).ToArray()));
+                }
+                if (command[0] == "PrintEven")
+                {
+                    Console.WriteLine(string.Join(" ", list.Where(x => x % 2 == 0).ToArray()));
+                }
+                if (command[0] == "GetSum")
+                {
+                    Console.WriteLine(list.Sum());
+                }
+                if (command[0] == "Filter")
+                {
+                    if (command[1] == "<")
+                    {
+                        Console.WriteLine(string.Join(" ", list.Where(x => x < int.Parse(command[2]))));
+                    }
+                    else if (command[1] == ">")
+                    {
+                        Console.WriteLine(string.Join(" ", list.Where(x => x > int.Parse(command[2]))));
+                    }
+                    else if (command[1] == "<=")
+                    {
+                        Console.WriteLine(string.Join(" ", list.Where(x => x <= int.Parse(command[2]))));
+                    }
+                    else if (command[1] == ">=")
+                    {
+                        Console.WriteLine(string.Join(" ", list.Where(x => x >= int.Parse(command[2]))));
+                    }
+                }
+
+                command = Console.ReadLine().Split(' ').ToList();
+            }
+
+            if (isChanged)
+            {
+                Console.WriteLine(string.Join(" ", list));
+            }
+        }
+    }
+}
+
---
+++ Lists/MergingLists/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergingLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstArr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> secondArr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            MergingLists(firstArr, secondArr);
+        }
+
+        static void MergingLists(List<int> firstArr, List<int> secondArr)
+        {
+            int length = Math.Abs(firstArr.Count+secondArr.Count);
+            List<int> finalArr = new List<int>(length);
+
+            if (firstArr.Count <= secondArr.Count)
+            {
+                for(int i = 0; i < firstArr.Count; i++)
+                {
+                    finalArr.Add(firstArr[i]);
+                    finalArr.Add(secondArr[i]);
+                }
+                
+                for(int j = firstArr.Count; j< secondArr.Count; j++)
+                {
+                    finalArr.Add(secondArr[j]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < secondArr.Count; i++)
+                {
+                    finalArr.Add(firstArr[i]);
+                    finalArr.Add(secondArr[i]);
+                }
+
+                for (int j = secondArr.Count; j < firstArr.Count; j++)
+                {
+                    finalArr.Add(firstArr[j]);
+                }
+            }
+
+            Console.WriteLine(String.Join(' ', finalArr));
+        }
+    }
+}
