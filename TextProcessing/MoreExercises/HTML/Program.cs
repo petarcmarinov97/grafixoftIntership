cba5aeb2036eb8b23cbd6f3aea8a0a3b1f18e051
Petar Marinov <petar.marinov@grafixoft.com>
2021-11-24 12:16:06 +0200

    HTML and TreasureFinder are Done from TextProcessing-More Exercises

---
+++ TextProcessing/MoreExercises/HTML/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HTML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string titleInput = Console.ReadLine();
+            string articleInput = Console.ReadLine();
+            List<string> comments = new List<string>();
+
+            string comment = Console.ReadLine();
+
+            while(comment!="end of comments")
+            {
+                comments.Add(comment);
+
+                comment = Console.ReadLine();
+            }
+
+            Console.Write($"<h1>\n  {titleInput}\n</h1>\n");
+            Console.Write($"<article>\n {articleInput}\n</article>\n");
+            foreach(string item in comments)
+            {
+                Console.Write($"<div>\n {item}\n</div>\n");
+            }
+        }
+    }
+}
---
+++ TextProcessing/MoreExercises/TreasureFinder/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace TreasureFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            string input = Console.ReadLine();
+
+            char[] currChars = new char[input.Length];
+            StringBuilder temp = new StringBuilder();
+            int result = 0;
+            int count = 0;
+
+            while (input != "find")
+            {
+                currChars = input.ToCharArray();
+
+                for(int i =0; i < currChars.Length; i++)
+                {
+                    if(count < numbers.Length)
+                    {
+                        result = (int)currChars[i] - numbers[count];
+                        temp.Append((char)result);
+                        count++;
+                    }
+                    else
+                    {
+                        count = 0;
+                        result = (int)currChars[i] - numbers[count];
+                        temp.Append((char)result);
+                        count++;
+                    }
+                }
+                count = 0;
+
+                int typeStartIndex = temp.ToString().IndexOf('&');
+                int typeEndIndex = temp.ToString().LastIndexOf('&');
+                string treasureType = temp.ToString().Substring(typeStartIndex + 1, typeEndIndex - typeStartIndex - 1);
+
+                int coordinatesStartIndex = temp.ToString().IndexOf('<');
+                int coordinatesEndIndex = temp.ToString().IndexOf('>');
+                string coordinates = temp.ToString().Substring(coordinatesStartIndex + 1, coordinatesEndIndex - coordinatesStartIndex - 1);
+
+                Console.WriteLine($"Found {treasureType} at {coordinates}");
+
+                temp = new StringBuilder();
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
