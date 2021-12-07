28ccd90e70779fb02a15567764da439371485a00
Petar Marinov <petar.marinov@grafixoft.com>
2021-12-07 16:55:16 +0200

    Bombs, Miner, KnightGame are Done

---
+++ MultidimensionalArrays/Exercise/Knights/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace Knights
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[][] matrix = new char[n][];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                char[] row = Console.ReadLine().ToCharArray();
+                matrix[i] = new char[n];
+                matrix[i] = row;
+            }
+
+
+            int currentKnightsInDanger = 0;
+            int maxKnightsInDanger = -1;
+            int mostDangerousRow = 0;
+            int mostDangerousColumn = 0;
+            int countRemoves = 0;
+
+            while (true)
+            {
+                if (n < 3)
+                {
+                    Console.WriteLine("0");
+                    return;
+                }
+
+                for (int rowIndex = 0; rowIndex < matrix.Length; rowIndex++)
+                {
+                    for (int columnIndex = 0; columnIndex < matrix[rowIndex].Length; columnIndex++)
+                    {
+                        if (matrix[rowIndex][columnIndex].Equals('K'))
+                        {
+                           currentKnightsInDanger = GetKnightsInDanger(rowIndex, columnIndex, matrix, currentKnightsInDanger);
+                        }
+
+                        if (currentKnightsInDanger > maxKnightsInDanger)
+                        {
+                            maxKnightsInDanger = currentKnightsInDanger; //setting the newMostDangerousKnight who needs to be removed;
+                            mostDangerousRow = rowIndex; //setting the cordinates - row
+                            mostDangerousColumn = columnIndex; //setting the cordinates - column
+                        }
+
+                        currentKnightsInDanger = 0; //Setting to 0 before the next loop 
+                    }
+                }
+
+                if (maxKnightsInDanger != 0)
+                {
+                    matrix[mostDangerousRow][mostDangerousColumn] = 'O'; //removing the most dangerous knight
+                    countRemoves++; // Increasing the count of removed knights
+                    maxKnightsInDanger = 0;
+                }
+                else
+                {
+                    Console.WriteLine(countRemoves);
+                    return;
+                }
+            }
+        }
+       
+        public static int GetKnightsInDanger(int rowIndex, int columnIndex, char [][]matrix, int currentKnightsInDanger)
+        {
+            //vertical-top left
+            if (ReturnIsCellInMatrix(rowIndex - 2, columnIndex - 1, matrix))
+            {
+                if (matrix[rowIndex - 2][columnIndex - 1].Equals('K'))
+                {
+                    currentKnightsInDanger++;
+                }
+            }
+
+            //top-vertical right
+            if (ReturnIsCellInMatrix(rowIndex - 2, columnIndex + 1, matrix))
+            {
+                if (matrix[rowIndex - 2][columnIndex + 1].Equals('K'))
+                {
+                    currentKnightsInDanger++;
+                }
+            }
+
+            // bot-vertical and left
+            if (ReturnIsCellInMatrix(rowIndex + 2, columnIndex - 1, matrix))
+            {
+                if (matrix[rowIndex + 2][columnIndex - 1].Equals('K'))
+                {
+                    currentKnightsInDanger++;
+                }
+            }
+
+            // bot-vertical and right
+            if (ReturnIsCellInMatrix(rowIndex + 2, columnIndex + 1, matrix))
+            {
+                if (matrix[rowIndex + 2][columnIndex + 1].Equals('K'))
+                {
+                    currentKnightsInDanger++;
+                }
+            }
+
+            // horizontal left and up
+            if (ReturnIsCellInMatrix(rowIndex - 1, columnIndex - 2, matrix))
+            {
+                if (matrix[rowIndex - 1][columnIndex - 2].Equals('K'))
+                {
+                    currentKnightsInDanger++;
+                }
+            }
+
+            // horizontal right and up
+            if (ReturnIsCellInMatrix(rowIndex - 1, columnIndex + 2, matrix))
+            {
+                if (matrix[rowIndex - 1][columnIndex + 2].Equals('K'))
+                {
+                    currentKnightsInDanger++;
+                }
+            }
+
+            // horizontal left and down
+            if (ReturnIsCellInMatrix(rowIndex + 1, columnIndex - 2, matrix))
+            {
+                if (matrix[rowIndex + 1][columnIndex - 2].Equals('K'))
+                {
+                    currentKnightsInDanger++;
+                }
+            }
+
+            // horizontal right and down
+            if (ReturnIsCellInMatrix(rowIndex + 1, columnIndex + 2, matrix))
+            {
+                if (matrix[rowIndex + 1][columnIndex + 2].Equals('K'))
+                {
+                    currentKnightsInDanger++;
+                }
+            }
+
+            return currentKnightsInDanger;
+        }
+        public static bool ReturnIsCellInMatrix(int row, int col, char[][] matrix)
+        {
+            return (0 <= row && row < matrix.Length && 0 <= col && col < matrix[row].Length);
+        }
+    }
+}
---
+++ MultidimensionalArrays/Exercise/Miner/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+
+namespace Miner
+{
+    internal class Program
+    {
+
+        static char[,] matrix;
+        static int minorRow;
+        static int minorCol;
+        static int coals;
+        static bool isGameOver;
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            string[] directions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            matrix = new char[size, size];
+            ReadMatrix();
+
+            foreach (var currentDirection in directions)
+            {
+                switch (currentDirection)
+                {
+                    case "left":
+                        Move(0, -1);
+                        break;
+                    case "right":
+                        Move(0, 1);
+                        break;
+                    case "up":
+                        Move(-1, 0);
+                        break;
+                    case "down":
+                        Move(1, 0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (coals > 0 && !isGameOver)
+            {
+                Console.WriteLine($"{coals} coals left. ({minorRow}, {minorCol})");
+            }
+        }
+
+        private static void ReadMatrix()
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] input = Console.ReadLine()
+                    .Split()
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+
+                    if (matrix[row, col] == 's')
+                    {
+                        minorRow = row;
+                        minorCol = col;
+                    }
+                    if (matrix[row, col] == 'c')
+                    {
+                        coals++;
+                    }
+                }
+            }
+        }
+
+        private static bool IsInside(int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1);
+        }
+        private static void Move(int row, int col)
+        {
+            if (IsInside(minorRow + row, minorCol + col))
+            {
+                minorRow += row;
+                minorCol += col;
+
+                if (matrix[minorRow, minorCol] == 'e')
+                {
+                    Console.WriteLine($"Game over! ({minorRow}, {minorCol})");
+                    isGameOver = true;
+                    return;
+                }
+
+                if (matrix[minorRow, minorCol] == 'c')
+                {
+                    coals--;
+                    matrix[minorRow, minorCol] = '*';
+                    if (coals == 0)
+                    {
+                        Console.WriteLine($"You collected all coals! ({minorRow}, {minorCol})");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
